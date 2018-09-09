11e9abf3db4cc52e43e6c05c5264564c63bcf860
Naira Lalayan <korosu.o.korosu@gmail.com>
2018-09-09 13:52:47 +0300

    add frames

---
+++ js/main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const RIGHT_ARROW = 37;
+const LEFT_ARROW = 39;
+
+const mainElement = document.querySelector(`#main`);
+
+const selectSlide = (element) => {
+  mainElement.innerHTML = ``;
+  mainElement.appendChild(element.cloneNode(true));
+};
+
+const screens = Array.from(document.querySelectorAll(`template`)).
+map((it) => it.content);
+
+let current = 0;
+const select = (index) => {
+  index = index < 0 ? screens.length - 1 : index;
+  index = index >= screens.length ? 0 : index;
+  current = index;
+  selectSlide(screens[current]);
+};
+
+document.addEventListener(`keydown`, (evt) => {
+  switch (evt.keyCode) {
+    case RIGHT_ARROW:
+      select(current + 1);
+      break;
+    case LEFT_ARROW:
+      select(current - 1);
+      break;
+  }
+});
+
+select(0);
