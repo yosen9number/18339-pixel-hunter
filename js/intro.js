2ea81e75c71021681f6a78476bcdf33e180ddeeb
Naira Lalayan <korosu.o.korosu@gmail.com>
2018-09-09 14:52:47 +0300

    добавила модули

---
+++ js/intro.js
@@ -0,0 +1,10 @@
+const moduleIntro = getElementFromTemplate(`
+    <template id="intro">
+        <section class="intro">
+        <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
+    <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+    </section>
+    </template>
+`);
+
+export default moduleIntro;
